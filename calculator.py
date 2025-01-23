e25b19a3467f4a98425cf2830ebcbc71debf9525
adstump <sobutayfirat905@gmail.com>
2025-01-23 10:39:14 +0000

    Pending changes exported from your codespace

---
+++ calculator.py
@@ -0,0 +1,17 @@
+from operations import add, subtract, multiply, divide
+a = 10
+class Calculator:
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return add(x, y)
+
+    def subtract(self, x, y):
+        return subtract(x, y)
+
+    def multiply(self, x, y):
+        return multiply(x, y)
+
+    def divide(self, x, y):
+        return divide(x, y)
